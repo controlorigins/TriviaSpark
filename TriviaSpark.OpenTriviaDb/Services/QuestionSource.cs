4e4c49b28e673f504b3892efb52ded9abb4b57f8
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-03-12 10:58:46 -0500

    nLog basic implementation

--- TriviaSpark.OpenTriviaDb/Services/QuestionSource.cs
+++ TriviaSpark.OpenTriviaDb/Services/QuestionSource.cs
@@ -1,6 +1,6 @@
 ﻿using HttpClientDecorator.Interfaces;
 using HttpClientDecorator.Models;
-using System;
+using Microsoft.Extensions.Logging;
 using TriviaSpark.Core.Extensions;
 using TriviaSpark.Core.Interfaces;
 using TriviaSpark.Core.Questions;
@@ -11,10 +11,12 @@ namespace TriviaSpark.OpenTriviaDb.Services
     public class OpenTriviaDbQuestionSource : IQuestionSourceAdapter
     {
         private readonly IHttpGetCallService _httpClientService;
+        private readonly ILogger<OpenTriviaDbQuestionSource> _logger;
 
-        public OpenTriviaDbQuestionSource(IHttpGetCallService httpGetCallService)
+        public OpenTriviaDbQuestionSource(ILogger<OpenTriviaDbQuestionSource> logger, IHttpGetCallService httpGetCallService)
         {
             _httpClientService = httpGetCallService;
+            _logger = logger;
         }
 
         /// <summary>
@@ -24,21 +26,23 @@ namespace TriviaSpark.OpenTriviaDb.Services
         /// <returns>A sequence of QuestionModel objects.</returns>
         /// <exception cref="ArgumentNullException">Thrown if the trivia parameter is null.</exception>
         /// <exception cref="ArgumentException">Thrown if the trivia parameter contains null or empty elements.</exception>
-        private static IEnumerable<QuestionModel> Create(IEnumerable<Trivia>? trivia)
+        private IEnumerable<QuestionModel> Create(IEnumerable<Trivia>? trivia)
         {
             if (trivia == null)
             {
+                _logger.LogError("The trivia parameter cannot contain null elements.");
                 throw new ArgumentNullException(nameof(trivia), "The trivia parameter cannot be null.");
             }
 
             if (trivia.Any(t => t == null))
             {
+                _logger.LogError("The trivia parameter cannot contain null elements.");
                 throw new ArgumentException("The trivia parameter cannot contain null elements.", nameof(trivia));
             }
             return trivia.Select(t => Create(t));
         }
 
-        private static QuestionModel Create(Trivia trivia)
+        private QuestionModel Create(Trivia trivia)
         {
             QuestionModel questionModel = new QuestionModel
             {
@@ -68,7 +72,7 @@ namespace TriviaSpark.OpenTriviaDb.Services
 
             if (results?.ResponseResults?.results is null)
             {
-                // Log Error
+                _logger.LogError("No results returned from OpenTriviaDb");
             }
             questionList.AddRange(Create(results?.ResponseResults?.results));
             return questionList;
